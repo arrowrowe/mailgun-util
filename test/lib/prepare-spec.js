766034fec7d671d4901b1258269e97ae3358d265
arrowrowe <arrowrowe@gmail.com>
2015-10-07 16:45:17 +0800

    Update test: check lib/prepare's file reading

--- test/lib/prepare-spec.js
+++ test/lib/prepare-spec.js
@@ -5,10 +5,16 @@ var prepare = require('../../lib/prepare');
 var fs = require('fs');
 
 describe('lib/prepare', function () {
+
   it('removes data.textPath and place the content of its file to data.text', function () {
-    sinon.stub(fs, 'readFileSync');
-    prepare({textPath: 'textPath'});
+    var data = {
+      textPath: 'textPath'
+    };
+    sinon.stub(fs, 'readFileSync').returns('text');
+    prepare(data);
     expect(fs.readFileSync.callCount).to.equal(1);
+    expect(data.text).to.equal('text');
     fs.readFileSync.restore();
   });
+
 });
